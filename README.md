87d3c65c93083ffa670ed70f3b288f1de9da47b1
joshua <joshuacarpenter08@gmail.com>
2026-09-23 16:29:05 -0400

    Added README and moved away from CLion CMake

---
+++ README.md
@@ -0,0 +1,12 @@
+# Enigma
+
+An Enigma Machine emulater written in C++
+
+
+## Usage
+
+```bash
+cmake -S . -B build/
+make -C build/
+./build/Enigma
+```
--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,13 @@ using namespace std;
 
 int main()
 {
-	// std::cout << Rotor::charToInt('A');
-	// std::cout << Rotor::charToInt('a');
-	// std::cout << Rotor::charToInt('d');
-	// Rotor rotor(ROTOR1);
-	// char enc = rotor.encryptChar('A');
-	// cout << enc << endl;
-	// enc = rotor.encryptChar(enc);
-	// cout << enc << endl;
-	// enc = rotor.decryptChar('g');
-	// cout << enc << endl;
-	// rotor.rotate();
-	// cout << endl << "Rotate" << endl;
-	//
-	// enc = rotor.encryptChar('A');
-	// cout << enc << endl;
-	// enc = rotor.encryptChar(enc);
-	// cout << enc << endl;
-	// enc = rotor.decryptChar('g');
-	// cout << enc << endl;
-	//
-	// cout << endl << -1%26 << endl << 27 % 26;
-
 	Enigma machine;
 	constexpr int order[3] = {1, 2, 3};
 	constexpr int plugs[3] = {0, 0, 0};
 	// // constexpr char board[10][2] = {};
 	//
 	machine.changeSettings(order, plugs);
-	string encrypted = machine.encryptString("This is encrypted text");
+	const string encrypted = machine.encryptString("This is encrypted text");
 	cout << encrypted << endl;
 	machine.changeSettings(order, plugs);
 	cout << machine.encryptString(encrypted) << endl;
